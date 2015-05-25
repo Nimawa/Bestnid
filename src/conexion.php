b33f2ca81e3f44e5f11fb5246e7c7af2b2d49b74
pitu224 <pitu224@hotmail.com>
2015-05-25 19:23:10 -0300

    eliminar

--- src/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-	function conectar(){
-		$conexion=mysql_connect("http://bestnid.ddns.net","bestnid","bestnid123456") or die("Problemas en la conexion");
-		mysql_select_db("bestnid",$conexion) or  die("Problemas en la selección de la base de datos");
-		mysql_query("SET NAMES 'utf8'");
-	return $conexion;
-							}
-?>
