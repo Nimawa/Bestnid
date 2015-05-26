8d57b6ed7be56c037eb8d5f73ae8606ecbd894c9
pitu224 <pitu224@hotmail.com>
2015-05-26 20:47:19 -0300

    Correccion alta usuario

---
+++ src/altaUsuario.php
@@ -0,0 +1,21 @@
+<?php
+$nombre=$_REQUEST['nombre'];
+$apellido=$_REQUEST['apellido'];
+$dni=$_REQUEST['dni'];
+$telefono=$_REQUEST['telefono'];
+$calle=$_REQUEST['calle'];
+$nro=$_REQUEST['nro'];
+$piso=$_REQUEST['piso'];
+$depto=$_REQUEST['depto'];
+$ciudad=$_REQUEST['ciudad'];
+$pcia=$_REQUEST['pcia'];
+$email=$_REQUEST['email'];
+$pass=$_REQUEST['pass2'];
+
+require 'conexion.php';
+$conexion=conectar();
+
+mysql_query("	INSERT INTO usuario (mail,dni,nombre,apellido,telefono,calle,nro,piso,depto,ciudad,pcia,pass)
+             	VALUES ('$email','$dni','$nombre','$apellido','$telefono','$calle','$nro','$piso','$depto','$ciudad','$pcia','$pass')",$conexion) or die("Problemas en el select:".mysql_error());
+
+?>
--- src/solicitudAltaDeUsuario.php
+++ src/solicitudAltaDeUsuario.php
@@ -31,97 +31,97 @@
                     
                 </div>
                 <div class="col-xs-6 col-md-8">
-                    <form data-toggle="validator" class="form-horizontal" role="form" >
+                    <form action="altaUsuario.php" method="post" data-toggle="validator" class="form-horizontal"  id="formularioAltaUsuario" >
                         Aclaracion: Los campos con (*) son obligatorios <br><br>
                         <div class="form-group">
                             <label for="nombre" class="col-lg-2 control-label">Nombre: *</label>
                             <div class="col-lg-4">
-                              <input type="text" class="form-control" id="nombre" required placeholder="Nombre" data-error=" Ingrese un dato Valido!">
+                              <input type="text" class="form-control" id="nombre" name="nombre" required placeholder="Nombre" data-error=" Ingrese un dato Valido!">
                               <div class="help-block with-errors"></div>   
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="apellido" class="col-lg-2 control-label">Apellido: *</label>
                             <div class="col-lg-4">
-                              <input type="text" class="form-control" id="apellido" required placeholder="Apellido" data-error=" Ingrese un dato Valido!">
+                              <input type="text" class="form-control" id="apellido" name="apellido" required placeholder="Apellido" data-error=" Ingrese un dato Valido!">
                               <div class="help-block with-errors"></div>   
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="dni" class="col-lg-2 control-label">DNI: * </label>
                             <div class="col-lg-4">
-                                <input type="text" class="form-control" id="dni" required placeholder="Ej. 12456879" data-error=" Ingrese un dato Valido!">
+                                <input type="text" class="form-control" id="dni" name="dni" required placeholder="Ej. 12456879" data-error=" Ingrese un dato Valido!">
                                 <div class="help-block with-errors"></div> 
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="telefono" class="col-lg-2 control-label">Telefono: * </label>
                             <div class="col-lg-4">
-                              <input type="text" class="form-control" id="telefono" required placeholder="Ej. 221 - 154568789" data-error=" Ingrese un dato Valido!">
+                              <input type="text" class="form-control" id="telefono" name="telefono" required placeholder="Ej. 221 - 154568789" data-error=" Ingrese un dato Valido!">
                               <div class="help-block with-errors"></div> 
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="calle" class="col-lg-2 control-label">Calle: * </label>
                             <div class="col-lg-3">
-                              <input type="text" class="form-control" id="calle" required placeholder="Calle" data-error=" Ingrese un dato Valido!" >
+                              <input type="text" class="form-control" id="calle" name="calle" required placeholder="Calle" data-error=" Ingrese un dato Valido!" >
                             </div>
                             
                             <label for="nro" class="col-lg-2 control-label">Nro: * </label>
                             <div class="col-lg-2">
-                                <input type="text" class="form-control" id="nro" required placeholder="Ej. 1234" data-error=" Ingrese un dato Valido!">
+                                <input type="text" class="form-control" id="nro" name="nro" required placeholder="Ej. 1234" data-error=" Ingrese un dato Valido!">
                                 <div class="help-block with-errors"></div> 
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="piso" class="col-lg-2 control-label">Piso:  </label>
                             <div class="col-lg-2">
-                                <input type="number" class="form-control" id="piso" placeholder="Ej. A" >
+                                <input type="number" class="form-control" id="piso" name="piso" placeholder="Ej. A" >
                             </div>
                             <label for="depto" class="col-lg-2 control-label">Depto.:  </label>
                             <div class="col-lg-2">
-                              <input type="text" class="form-control" id="depto" placeholder="Ej. 1" >
+                              <input type="text" class="form-control" id="depto" name="depto" placeholder="Ej. 1" >
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="ciudad" class="col-lg-2 control-label">Ciudad: * </label>
                             <div class="col-lg-4">
-                              <input type="text" class="form-control" id="ciudad" required placeholder="Ciudad" data-error=" Ingrese un dato Valido!">
+                              <input type="text" class="form-control" id="ciudad" name="ciudad" required placeholder="Ciudad" data-error=" Ingrese un dato Valido!">
                               <div class="help-block with-errors"></div> 
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="telefono" class="col-lg-2 control-label">Pcia: * </label>
+                            <label for="pcia" class="col-lg-2 control-label">Pcia: * </label>
                             <div class="col-lg-4">
-                                <input type="text" class="form-control" id="telefono" required placeholder="Provincia" data-error=" Ingrese un dato Valido!">
+                                <input type="text" class="form-control" id="pcia" name="pcia" required placeholder="Provincia" data-error=" Ingrese un dato Valido!">
                                 <div class="help-block with-errors"></div> 
                             </div>
                         </div>
                         Aclaracion: Su correo electronico sera su nombre de usuario<br><br>
                         <div class="form-group">
-                            <label for="telefono" class="col-lg-4 control-label">Email: * </label>
+                            <label for="email" class="col-lg-4 control-label">Email: * </label>
                             <div class="col-lg-4">
-                                <input type="email" class="form-control" id="telefono" required placeholder="ejemplo@mail.com" data-error=" Ingrese una direccion de correo electronico Valida!">
+                                <input type="email" class="form-control" id="email" name="email" required placeholder="ejemplo@mail.com" data-error=" Ingrese una direccion de correo electronico Valida!">
                                 <div class="help-block with-errors"></div> 
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="pass1" class="col-lg-4 control-label">Contraseña: * </label>
                             <div class="col-lg-4">
-                                <input type="password" class="form-control" id="pass1" placeholder="" required>
+                                <input type="password" class="form-control" id="pass1" name="pass1" placeholder="" required>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="pass2" class="col-lg-4 control-label">Repetir Contraseña: * </label>
                             <div class="col-lg-4">
-                                <input type="password" class="form-control" id="pass2" placeholder="" required data-match="#pass1" data-error=" Las contraseñas no coinciden!">
+                                <input type="password" class="form-control" id="pass2" name="pass2" placeholder="" required data-match="#pass1" data-error=" Las contraseñas no coinciden!">
                                 <div class="help-block with-errors"></div>
                             </div>
                         </div>
                         <div class="form-group" >
                             <div class="col-lg-10">
                               <button type="button" class="btn btn-default" style=" margin-left: 100px;">Cancelar</button>
-                              <button type="submit" class="btn btn-danger" style=" margin-left: 20px;">Aceptar</button>
+                              <input type="submit" class="btn btn-danger" style=" margin-left: 20px;" value="Aceptar">
                             </div>
                         </div>
                        
@@ -139,4 +139,6 @@
     
   </body>
 </html>
+
+
 
