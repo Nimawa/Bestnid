8b956e33774cd0c02d0b011fb15b196858219c83
Marcelo Delledonne <Pitu224@hotmail.com>
2015-05-29 20:25:01 -0300

    Areeglos vista

--- src/listarProductos.php
+++ src/listarProductos.php
@@ -1,13 +1,10 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Documento sin t&iacute;tulo</title>
-</head>
-<?php   
-    include 'head.php';
-	include 'navegacion.php';
-	?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <body> 
+    
+    <?php include 'head.php';?>
+    <?php include 'navegacion.php';?>
 <div class="col-xs-6 col-md-4">
                     <a style=" color: #000; font-style:italic; font-size: 20px; ">
                         <strong> BESTNID </strong>
