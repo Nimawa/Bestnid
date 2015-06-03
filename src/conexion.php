54ba3323da847b814e89677fbf0e2886272caee3
Marcelo Delledonne <Pitu224@hotmail.com>
2015-06-03 15:38:36 -0300

    Conexion

--- src/conexion.php
+++ src/conexion.php
@@ -1,6 +1,6 @@
 <?php
 	function conectar(){
-		$conexion=mysql_connect ("localhost","root","") or die("Problemas en la conexion");
+		$conexion=mysql_connect ("181.170.74.137","bestnid","bestnid123456") or die("Problemas en la conexion");
 		mysql_select_db("bestnid",$conexion) or  die("Problemas en la selección de la base de datos");
 		mysql_query("SET NAMES 'utf8'");
 	return $conexion;
