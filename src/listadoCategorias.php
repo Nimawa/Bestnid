b8b3526ba701a570dda997c30aad17e7f9feb535
Marcelo Delledonne <Pitu224@hotmail.com>
2015-06-05 17:40:04 -0300

    Listado de categorias

--- src/listadoCategorias.php
+++ src/listadoCategorias.php
@@ -6,8 +6,17 @@
             $conexion= conectar();
           $cat=mysql_query(" SELECT cat.nombre, cat.id, count( pub.id ) as cantidad FROM categoria AS cat JOIN publicacion AS pub WHERE cat.id = pub.id_categoria AND pub.baja = 'false' GROUP BY cat.nombre",$conexion)or die("problema de select".mysql_error());
           while($categoria=mysql_fetch_array($cat)){
-            ?>
-            <a href="#" class="list-group-item" onclick="window.location.href='filtrador.php?categoria=<?php echo $categoria['id'];?>'"><span class="badge"><?php echo $categoria['cantidad']; ?> </span><?php echo $categoria['nombre']; ?> </a><?php        
-          } 
+              if(isset($_REQUEST['categoria']) and $_REQUEST['categoria']==$categoria['id']){
+                ?><a href="#" class="list-group-item active" onclick="window.location.href='filtrador.php?categoria=<?php echo $categoria['id'];?>'"><span class="badge"><?php echo $categoria['cantidad']; ?> </span><?php echo $categoria['nombre']; ?> </a><?php        
+                }
+                else{
+                    ?><a href="#" class="list-group-item" onclick="window.location.href='filtrador.php?categoria=<?php echo $categoria['id'];?>'"><span class="badge"><?php echo $categoria['cantidad']; ?> </span><?php echo $categoria['nombre']; ?> </a><?php        
+
+                }
+          }
+
+              
+          
     ?>       
 </div>
+
