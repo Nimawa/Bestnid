0b48d4da8ba7db263a477e40cd4560212a12167f
Nicolás Zárate <nico.zarate1984@gmail.com>
2015-06-13 18:43:15 -0300

    alta categoria modificacion

--- admin/altaCategoriaFuncionalidad.php
+++ admin/altaCategoriaFuncionalidad.php
@@ -8,13 +8,13 @@ $conexion=conectar();
 $verificaCategoria=mysql_query(" SELECT count(nombre) as cantidad from categoria where nombre='$nombreCategoria' ",$conexion) or die("Problemas en el select:".mysql_error());
 $cantNombreCategoria=mysql_fetch_array($verificaCategoria);
 if($cantNombreCategoria['cantidad']==1){
-    echo "la categoria ingresado se encuentra registrado";
+   header("location: panel.php?c=2 ");
 }else
 {
 
 mysql_query("	INSERT INTO categoria (id,nombre)
              	VALUES (null,'$nombreCategoria')",$conexion) or die("Problemas en el select:".mysql_error());
 
-header("location: panel.php ");
+header("location: panel.php?c=2 ");
 }
 ?>
