12941c95c50eb6db78fefaa91673ab01d0e96ff1
walterlu <walterlu@argentina.com>
2015-06-12 21:17:36 -0300

    ahora si esta terminado

--- src/areaUsuario.php
+++ src/areaUsuario.php
@@ -4,8 +4,10 @@
                       <a href="#" class="list-group-item " ><span class="badge"></span>-- Publicaciones Activas</a>
                       <a href="#" class="list-group-item " ><span class="badge"></span>-- Publicaciones Finalizadas</a>
                       <a href="#" class="list-group-item " ><span class="badge"></span>-- Publicaciones Eliminadas</a>
-                      <form name="consulta" action="" onsubmit="mostrarOfertasActivas('ofertasActivas.php'); return false">
+                    <!--  <form name="consulta" action="" onsubmit="mostrarOfertasActivas('ofertasActivas.php'); return false">
                       <input type="submit" class="list-group-item" class="badge" value="-- Mis Ofertas Activas">
-                      </form>|
+                      </form>|-->
+                      <a onclick="mostrarOfertasActivas('ofertasActivas.php'); return false" class="list-group-item" ><span class="badge"></span>-- Mis Ofertas Terminadas</a>       
+
                       <a href="#" class="list-group-item" ><span class="badge"></span>-- Mis Ofertas Terminadas</a>       
 </div>
--- src/ofertasActivas.php
+++ src/ofertasActivas.php
@@ -7,12 +7,9 @@
 	$conexion=conectar();
 	$id_usuario=$_SESSION["id"]; 
 
-
-	$reg=mysql_query(" Select * from publicacion WHERE id_usuario=$id_usuario AND baja=false",$conexion)or die("problema de select".mysql_error());
+	$reg=mysql_query(" Select * FROM oferta AS ofer INNER JOIN publicacion AS publi WHERE ofer.id_usuario=$id_usuario AND ofer.ganador='false' AND publi.baja=false AND ofer.id_publicacion=publi.id" ,$conexion)or die("problema de select".mysql_error());
 	require 'imprimirPublicacion.php';
 	imprimirPublicacion($reg,$conexion);
 	mysql_close($conexion);
-//	$_SESSION['$a']=(" Select * from publicacion a ");
-	
-
+	$_SESSION['$a']=(" Select * from publicacion a ");
 ?> 
