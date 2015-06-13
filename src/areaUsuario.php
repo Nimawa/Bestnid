85582d3c5cb81bf86984b51b9be338daa5619068
Marcelo Delledonne <Pitu224@hotmail.com>
2015-06-12 22:05:18 -0300

    Correccion menu

--- src/areaUsuario.php
+++ src/areaUsuario.php
@@ -1,13 +1,14 @@
 <div class="list-group">
                       <a href="#" class="list-group-item active"></span><strong>MI CUENTA</strong></a>
                       <a href="#" class="list-group-item " ><span class="badge"></span><strong>Mis Publicaciones</strong></a>
-                      <a href="#" class="list-group-item " ><span class="badge"></span>-- Publicaciones Activas</a>
-                      <a href="#" class="list-group-item " ><span class="badge"></span>-- Publicaciones Finalizadas</a>
-                      <a href="#" class="list-group-item " ><span class="badge"></span>-- Publicaciones Eliminadas</a>
-                    <!--  <form name="consulta" action="" onsubmit="mostrarOfertasActivas('ofertasActivas.php'); return false">
-                      <input type="submit" class="list-group-item" class="badge" value="-- Mis Ofertas Activas">
-                      </form>|-->
-                      <a onclick="mostrarOfertasActivas('ofertasActivas.php'); return false" class="list-group-item" ><span class="badge"></span>-- Mis Ofertas Terminadas</a>       
-
-                      <a href="#" class="list-group-item" ><span class="badge"></span>-- Mis Ofertas Terminadas</a>       
+                      <a href="#" class="list-group-item " ><span class="badge"></span>- Publicaciones Activas</a>
+                      <a href="#" class="list-group-item " ><span class="badge"></span>- Publicaciones Finalizadas</a>
+                      <a href="#" class="list-group-item " ><span class="badge"></span>- Publicaciones Eliminadas</a>
+                      <a href="#" class="list-group-item " ><span class="badge"></span>- Ofertas Recibidas</a>
+                      <a href="#" class="list-group-item " ><span class="badge"></span><strong>Mis Preguntas</strong></a>
+                      <a href="#" class="list-group-item " ><span class="badge"></span>- Preguntas Recibidas</a>
+                      <a href="#" class="list-group-item " ><span class="badge"></span>- Preguntas Realizadas</a>
+                      <a href="#" class="list-group-item " ><span class="badge"></span><strong>Mis Ofertas</strong></a>
+                      <a href="#" class="list-group-item " onclick="mostrarOfertasActivas('ofertasActivas.php'); return false" ><span class="badge"></span>- Ofertas Activas</a>       
+                      <a href="#" class="list-group-item" ><span class="badge"></span>- Ofertas Finalizadas</a>       
 </div>
