705fad1bee2fb8c3e6b9f122686d06ad5b27ced3
Marcelo Delledonne <Pitu224@hotmail.com>
2015-06-18 00:02:11 -0300

    Fechas listado de usuarios

--- admin/listadoUsuariosRegistrados.php
+++ admin/listadoUsuariosRegistrados.php
@@ -3,33 +3,44 @@
 	$conexion=conectar();
 	$fecha1=$_REQUEST['fecha1'];
 	$fecha2=$_REQUEST['fecha2'];
-	$reg=mysql_query(" Select * FROM usuario where fecha_alta BETWEEN '$fecha1' and '$fecha2'  " ,$conexion)or die("problema de select".mysql_error());
-	$totalFilas=mysql_num_rows($reg);  
-    if($totalFilas==0){
+	
+    if ($fecha2 < $fecha1) {
     	?>
     		<div class="row" style="margin: 20px; background-color: #EEEEEE">
-	            <h4 class="col-xs-10 col-md-10">No existen resultados</h4>
+	            <h4 class="col-xs-10 col-md-10" style="color: red">ERROR! la segunda fecha debe ser mayor que la prtimera</h4>
 	        </div><br>
-	    <?php
-    }
-    else{
-	    ?>
-			<table class="table table-striped table-hover">
-				<thead>
-					<tr>
-						<th>Fecha de alta</th><th>Nombre de Usuario</th><th>Nombre</th><th>Apellido</th>
-					</tr>
-				</thead>	
-		<?php
-		while($registros=mysql_fetch_array($reg)){
-			?>
-					<tr>
-						<td><?php echo $registros['fecha_alta'] ?></td><td><?php echo $registros['mail'] ?></td><td><?php echo $registros['nombre'] ?></td><td><?php echo $registros['apellido'] ?></td>
-					</tr>
+	    <?php  
+    }else{
+    	$reg=mysql_query(" Select * FROM usuario where fecha_alta BETWEEN '$fecha1' and '$fecha2'  " ,$conexion)or die("problema de select".mysql_error());
+		$totalFilas=mysql_num_rows($reg);  
+	    if($totalFilas==0){
+	    	?>
+	    		<div class="row" style="margin: 20px; background-color: #EEEEEE">
+		            <h4 class="col-xs-10 col-md-10" >No existen resultados</h4>
+		        </div><br>
+		    <?php
+	    }
+	    else{
+		    ?>
+				<table class="table table-striped table-hover">
+					<thead>
+						<tr>
+							<th>Fecha de alta</th><th>Nombre de Usuario</th><th>Nombre</th><th>Apellido</th>
+						</tr>
+					</thead>	
 			<?php
+			while($registros=mysql_fetch_array($reg)){
+				?>
+						<tr>
+							<td><?php echo $registros['fecha_alta'] ?></td><td><?php echo $registros['mail'] ?></td><td><?php echo $registros['nombre'] ?></td><td><?php echo $registros['apellido'] ?></td>
+						</tr>
+				<?php
+			}
+			?></table><?php
 		}
-		?></table><?php
-	}
+    }
+  
+	
 	mysql_close($conexion);
 
 ?>
--- admin/verUsuariosRegistrados.php
+++ admin/verUsuariosRegistrados.php
@@ -39,22 +39,22 @@
 	            <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
 	            	<div  class="row">
 	                    <h3>
-	                        <strong style=""> Usuarios Registrados </strong>
-	                    </h3>
-                	</div>    
+	                        <strong style=""> Usuarios Registrados  </strong> 
+	                    </h3> Haga click en el campo para seleccionar una fecha.
+                	</div> <br>   
 	            	<div class="row">
 						<form method="POST" data-toggle="validator" class="form-horizontal"  id="buscarUsuarios" >
 	                        <div class="form-group col-lg-4 ">
 	                            <label for="nombre" class="col-lg-5 control-label">Desde: *</label>
 	                            <div class="col-lg-7">
-                  				<input type="text" class="form-control" required id="fecha1" name="fecha1" value="2015-01-01" >
+                  				<input type="text" class="form-control" required id="fecha1" name="fecha1" value="2015-01-01" pattern="(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])" onFocus="javascript:this.value=''" >
 	                              <div class="help-block with-errors"></div>   
 	                            </div>
 	                        </div>
 	                        <div class="form-group col-lg-4 ">
 	                            <label for="apellido" class="col-lg-5 control-label">Hasta: *</label>
 	                            <div class="col-lg-7">
-                  				<input type="text" class="form-control" required id="fecha2" name="fecha2" value="<?php echo date("Y-m-d")?>">
+                  				<input type="text" class="form-control" required id="fecha2" name="fecha2" value="<?php echo date("Y-m-d")?>" pattern="(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])" onFocus="javascript:this.value=''">
 	                              <div class="help-block with-errors"></div>   
 	                            </div>
 	                        </div>
