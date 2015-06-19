2008a56bd9b5b0b35c380f0542225c6229cfebab
Nicolás Zárate <nico.zarate1984@gmail.com>
2015-06-18 21:09:01 -0300

    publicaciones finalizadas

--- admin/areaAdministracion.php
+++ admin/areaAdministracion.php
@@ -2,7 +2,7 @@
                       <a href="#" class="list-group-item active"></span><strong>ADMINISTRACION</strong></a>
                       <a href="#" class="list-group-item " ><span class="badge"></span><strong>Publicaciones</strong></a>
                       <a href="#" class="list-group-item " ><span class="badge"></span>- Publicaciones Activas</a>
-                      <a href="#" class="list-group-item " ><span class="badge"></span>- Publicaciones Finalizadas</a>
+                      <a href="verPublicacionesFinalizadas.php" class="list-group-item " ><span class="badge"></span>- Publicaciones Finalizadas</a>
                       <a href="#" class="list-group-item " ><span class="badge"></span>- Publicaciones Eliminadas</a>
                       <a href="#" class="list-group-item " ><span class="badge"></span><strong>Categorias</strong></a>
                       <a href="altaCategoriaVisualizacion.php" class="list-group-item " ><span class="badge"></span>- Alta Categoria</a>
