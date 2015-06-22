4b97f30d584c01c8fc75a71613660cfcac5ebb1b
Nicolás Zárate <nico.zarate1984@gmail.com>
2015-06-22 14:12:16 -0300

    listo el listar productos
    ya no aparecen los finalizados

--- src/listarProductos.php
+++ src/listarProductos.php
@@ -1,6 +1,7 @@
 <?php 
 	include 'ordenarPublicaciones.php';
-	$reg=mysql_query(" Select * from publicacion ",$conexion)or die("problema de select".mysql_error());
+	$reg=mysql_query(" Select * from publicacion 
+	where fecha_fin > '$date'",$conexion)or die("problema de select".mysql_error());
 	require 'imprimirPublicacion.php';
 	imprimirPublicacion($reg,$conexion);
 	mysql_close($conexion);
