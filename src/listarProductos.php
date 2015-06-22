5340e0169ab72d86364c51bb629d61cb63736f86
Nicolás Zárate <nico.zarate1984@gmail.com>
2015-06-22 14:37:12 -0300

    modificacion pequeña
    la declaracion del date

--- src/listarProductos.php
+++ src/listarProductos.php
@@ -1,7 +1,8 @@
 <?php 
 	include 'ordenarPublicaciones.php';
-	$reg=mysql_query(" Select * from publicacion 
-	where fecha_fin > '$date'",$conexion)or die("problema de select".mysql_error());
+	$date=date("Y-m-d");
+	$reg=mysql_query(" Select * from publicacion
+	   where fecha_fin > '$date' ",$conexion)or die("problema de select".mysql_error());
 	require 'imprimirPublicacion.php';
 	imprimirPublicacion($reg,$conexion);
 	mysql_close($conexion);
