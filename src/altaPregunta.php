8ad46190a3b6e916e5076c0d2bda131b3e8d0017
Nicolás Zárate <nico.zarate1984@gmail.com>
2015-06-24 00:05:45 -0300

    alta pregunta
    ahora te envia al inicio sino iniciaste y te avisa

--- src/altaPregunta.php
+++ src/altaPregunta.php
@@ -1,6 +1,7 @@
 <?php
     require 'conexion.php';
 	require 'head.php';
+	
 	$conexion=conectar();
 	$idPublicacion= $_REQUEST['idPublicacion'];
 	$pregunta=$_REQUEST['pregunta'];
@@ -8,10 +9,13 @@
 	if (isset($_SESSION["id"]))
 	{
 	     $idPreguntador=$_SESSION["id"];
-	}
+	
 	    
 		 $altaPregunta=mysql_query(" INSERT INTO consulta(id, id_usuario, id_publicacion, pregunta, respuesta,fecha) 
 		 VALUES (null,'$idPreguntador','$idPublicacion','$pregunta',null,'$date')  ",$conexion)or die("problema de select".mysql_error());
 		  
   header("location: verPublicacion.php?idPublicacion=$idPublicacion");
+  
+  }else{
+  header("location: index.php?u=4");}
 ?>
