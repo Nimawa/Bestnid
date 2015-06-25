8d9c0811ed75e1d6fb0485ed6f85b64b345571a1
walterlu <walterlu@argentina.com>
2015-06-25 20:43:29 -0300

    arreglado el ofertas activas

--- src/ofertasActivas.php
+++ src/ofertasActivas.php
@@ -9,14 +9,10 @@
 	$id_usuario=$_SESSION["id"]; 
 	$fec_act=date("Y-m-d");  
 
-	$oferta=mysql_query("Select ofer.id as ido, ofer.id_publicacion, ofer.id_usuario as id_usuarioo, ofer.monto, ofer.descripcion as descripciono, ofer.ganador FROM oferta AS ofer INNER JOIN publicacion AS publi WHERE ofer.id_usuario=$id_usuario  AND publi.baja='false' AND ofer.id_publicacion=publi.id
-")or die("problema de select".mysql_error());//renombra las columnas sino queda descripcion dos veces
-	
-
-	$reg=mysql_query(" Select * FROM oferta AS ofer INNER JOIN publicacion AS publi WHERE ofer.id_usuario=$id_usuario AND publi.baja='false' AND ofer.id_publicacion=publi.id" ,$conexion)
+	$reg=mysql_query(" Select * FROM publicacion AS publi INNER JOIN oferta AS ofer WHERE ofer.id_usuario=$id_usuario AND ofer.id_publicacion=publi.id AND publi.fecha_fin > $fec_act" ,$conexion)
 	or die("problema de select".mysql_error());
-
-	if (mysql_num_rows($oferta)==0) {
+	
+	if (mysql_num_rows($reg)==0) {
 			?>
     		<div class="row" style="margin: 20px; background-color: #EEEEEE">
 	            <h4 class="col-xs-10 col-md-10">No existen resultados</h4>
@@ -29,19 +25,16 @@
 			or die("problema de select".mysql_error());
 			if ($registros['fecha_fin'] > $fec_act) {
 			imprimirPublicacion($publicacion,$conexion);
-				if ( $ofer=mysql_fetch_array($oferta)){
-				
-				?><div class="row" style="margin-top: 20px; background-color: #EEEEEE">
-				       	<h4 class="col-xs-10 col-md-10">Monto: $<?php echo $ofer['monto']?></h4>
-	                 	<h4 class="col-xs-10 col-md-10">Comentario: <?php echo $ofer["descripciono"]?></h4>
-	                 	<a class="pull-right" >  
-	               		<input  type="button" class="btn btn-primary btn-sm" style=" margin: 10px;" value="MODIFICAR OFERTA" onClick="window.location.href='modificarOferta.php?id=<?php echo $ofer['ido'];?>&idpubli=<?php echo $ofer['id_publicacion'];?>'">
-	               		</a> 
-	             </div><br>
-	             <?php
-	         	}
-	         }
-			}echo '<hr>';
+			?><div class="row" style="margin-top: 20px; background-color: #EEEEEE">
+			   	<h4 class="col-xs-10 col-md-10">Monto: $<?php echo $registros['monto']?></h4>
+	           	<h4 class="col-xs-10 col-md-10">Comentario: <?php echo $registros['descripcion']?></h4>
+	           	<a class="pull-right" >  
+	        	<input  type="button" class="btn btn-primary btn-sm" style=" margin: 10px;" value="MODIFICAR OFERTA" onClick="window.location.href='modificarOferta.php?id=<?php echo $registros['id'];?>&idpubli=<?php echo $registros['id_publicacion'];?>'">
+	        	</a> 
+	        </div><br>
+	        <?php	
+	        }
 	}
+	}echo '<hr>';
 	mysql_close($conexion);
 ?> 
