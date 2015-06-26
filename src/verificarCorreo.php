7df1f615eda3e794de9269993f1824e2049f758e
Marcelo Delledonne <Pitu224@hotmail.com>
2015-06-25 23:00:09 -0300

    color verificar correo

--- src/verificarCorreo.php
+++ src/verificarCorreo.php
@@ -8,11 +8,8 @@ $conexion=conectar();
 $verificaEmail=mysql_query(" SELECT count(id) as cantidad from usuario where mail='$email' ",$conexion) or die("Problemas en el select:".mysql_error());
 $cantEmail=mysql_fetch_array($verificaEmail);
 if($cantEmail['cantidad']==1){
-	echo "El correo ingresado se encuentra registrado";
-	?><script language="javascript">
-    document.getElementById(email).focus();
-    </script><?php
-}
+	?><a style='color: red'>El correo ingresado se encuentra registrado</a><?php
+	}
 
 
 ?>
