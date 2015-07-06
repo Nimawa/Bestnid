0d99ee7f81e9acec1c2042d75a3c5e411d856937
Nicolás Zárate <nico.zarate1984@gmail.com>
2015-07-06 17:07:08 -0300

    listados terminados

--- src/areaUsuario.php
+++ src/areaUsuario.php
@@ -1,14 +1,15 @@
 <div class="list-group">
                       <a href="#" class="list-group-item active"></span><strong>MI CUENTA</strong></a>
                       <a href="#" class="list-group-item " ><span class="badge"></span><strong>Mis Publicaciones</strong></a>
-                      <a href="#" class="list-group-item " ><span class="badge"></span>- Publicaciones Activas</a>
-                      <a href="#" class="list-group-item " ><span class="badge"></span>- Publicaciones Finalizadas</a>
-                      <a href="#" class="list-group-item " ><span class="badge"></span>- Publicaciones Eliminadas</a>
-                      <a href="#" class="list-group-item " onclick="mostrarResultados('ofertasRecibidas.php'); "><span class="badge"></span>- Ofertas Recibidas</a>
+                      <a href="#" class="list-group-item "  onclick="mostrarResultados('publicacionesActivas.php'); " ><span class="badge"></span>- Publicaciones Activas</a>
+                      <a href="#" class="list-group-item "  onclick="mostrarResultados('publicacionesFinalizadas.php'); " ><span class="badge"></span>- Publicaciones Finalizadas</a>
+                      <a href="#" class="list-group-item "  onclick="mostrarResultados('publicacionesEliminadas.php'); " ><span class="badge"></span>- Publicaciones Eliminadas</a>
+                  
                       <a href="#" class="list-group-item " ><span class="badge"></span><strong>Mis Preguntas</strong></a>
                       <a href="#" class="list-group-item " onclick="mostrarResultados('preguntasRecibidas.php'); "><span class="badge"></span>- Preguntas Recibidas</a>
                       <a href="#" class="list-group-item " onclick="mostrarResultados('preguntasRealizadas.php');" ><span class="badge"></span>- Preguntas Realizadas</a>
                       <a href="#" class="list-group-item " ><span class="badge"></span><strong>Mis Ofertas</strong></a>
+					      <a href="#" class="list-group-item " onclick="mostrarResultados('ofertasRecibidas.php'); "><span class="badge"></span>- Ofertas Recibidas</a>
                       <a href="#" class="list-group-item " onclick="mostrarResultados('ofertasActivas.php'); " ><span class="badge"></span>- Ofertas Activas</a>       
                       <a href="#" class="list-group-item " onclick="mostrarResultados('ofertasTerminadas.php'); " ><span class="badge"></span>- Ofertas Finalizadas</a>       
                       <a href="#" class="list-group-item " ><span class="badge"></span><strong>Mis Datos</strong></a>
