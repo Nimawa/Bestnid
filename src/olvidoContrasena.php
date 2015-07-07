e715def0ec1dffb4a8230e4043c20544eab6e150
Marcelo Delledonne <Pitu224@hotmail.com>
2015-07-07 13:37:29 -0300

    Recuperar contraseña

--- src/olvidoContrasena.php
+++ src/olvidoContrasena.php
@@ -42,7 +42,7 @@
                         <div class="form-group" >
                             <div class="col-lg-10">
                               <button type="button" class="btn btn-default" style=" margin-left: 100px;" onclick="window.location.href='panel.php'">Cancelar</button>
-                              <input type="submit" class="btn btn-danger" style=" margin-left: 20px;" value="Aceptar" onmouseover="verificarCorreo2();">
+                              <input type="submit" class="btn btn-danger" style=" margin-left: 20px;" value="Aceptar" ">
                             </div>
                         </div>
 
--- src/verificarcorreo2.php
+++ src/verificarcorreo2.php
@@ -5,11 +5,14 @@ $email=$_REQUEST['email'];
 require 'conexion.php';
 $conexion=conectar();
 
-$verificaEmail=mysql_query(" SELECT count(id) as cantidad from usuario where mail='$email' ",$conexion) or die("Problemas en el select:".mysql_error());
-$cantEmail=mysql_fetch_array($verificaEmail);
-if($cantEmail['cantidad']==0){
-	?><a style='color: #C54442'>El correo ingresado no se encuentra registrado</a><?php
+if($email!==''){
+	$verificaEmail=mysql_query(" SELECT count(id) as cantidad from usuario where mail='$email' ",$conexion) or die("Problemas en el select:".mysql_error());
+	$cantEmail=mysql_fetch_array($verificaEmail);
+	if($cantEmail['cantidad']==0){
+		?><a style='color: #C54442'>El correo ingresado no se encuentra registrado</a><?php
 	}
+}
+	
 
 
 ?>
