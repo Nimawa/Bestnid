34e2542545a1ad3426308e58d32b09af8389b72a
walterlu <walterlu@argentina.com>
2015-07-12 20:47:05 -0300

    modificado boton atras

--- src/busqueda.php
+++ src/busqueda.php
@@ -24,9 +24,22 @@
                 <div  class="col-xs-12 col-sm-6 col-md-8">
                     <form class="form-inline" role="form" action="filtrador.php" method="post">
                         <div class="form-group" >
-                            <button type="button" class="btn btn-danger" onclick="window.location.href='index.php'">
+                            <?php
+                                    if (isset($_SESSION['admin_id'])) {
+                                ?>
+                                    <button type="button" class="btn btn-danger" onclick="window.location.href='../admin/panel.php'">
+                                     <span class="glyphicon glyphicon-arrow-left"></span>
+                                      </button>   
+                                <?php
+                                }else {
+                                ?>
+                                <button type="button" class="btn btn-danger" onclick="window.location.href='index.php'">
                                 <span class="glyphicon glyphicon-arrow-left"></span>
                             </button>
+                                 <?php
+                                }
+                            ?>
+                            
                             <label for="campoBusqueda">Busqueda:</label>
                             <input type="text" class="form-control" id="campoBusqueda" name="campoBusqueda" placeholder=" Ingrese su busqueda..." style="margin-bottom: 3px;">
                             
